6e3fe085da997825c2fb1de473cc61fb074e5b46
Stuart Skeer <sskeer@aol.com>
2025-11-18 13:22:51 +0000

    updated index with router usage and import

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import Router from "./views/router.js";
 import cors from "cors";
 
-const port = process.env.PORT;
+const port = 3000;
 const app = express();
 
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
+app.use(Router);
 
 async function startServer() {
   try {
