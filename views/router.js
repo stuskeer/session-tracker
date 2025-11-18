8438dd1934a74f34418399608cf6516368418b69
Stuart Skeer <sskeer@aol.com>
2025-11-18 11:13:23 +0000

    added router

---
+++ views/router.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const Router = express.Router();
+
+Router.route("/session")
+  .get((req, res) => {
+    res.send(
+      "🤖 Session Route with GET method - this endpoint will get all of the session from the database"
+    );
+  })
+  .post((req, res) => {
+    res.send(
+      "🤖 Session Route with POST method - this endpoint will create a new session in the database"
+    );
+  });
+
+export default Router;
